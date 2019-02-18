6f2fb1f328438ecef301c86dce2ed9fa17714cc9
stomg7969 <stomg7969@gmail.com>
2019-02-17 21:19:23 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Nate, Kyung1"
+  end
+
+  get '/hometown' do
+    "My hometown is LIC, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Aruarian Dance by Nujabes."
+  end
+
 end
