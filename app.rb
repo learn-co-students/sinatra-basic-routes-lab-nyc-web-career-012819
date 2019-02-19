1ee1b0fa9e303e1a8ca0e6ca3f2c0a2f8a7f3d34
dren39 <dren391@gmail.com>
2019-02-19 15:23:38 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is dren"
+  end
+
+  get '/hometown' do
+    "My hometown is Somewhere"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is my voice"
+  end
 end
